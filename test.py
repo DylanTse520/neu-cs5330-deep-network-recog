46cb6a86a5d2c8bb73b94277c6108401b2c299c8
Dylan Tse <dylan052099@gmail.com>
2023-11-21 18:30:12 -0800

    feat: train network and test on original data

--- test.py
+++ test.py
@@ -5,29 +5,83 @@
 
 # import statements
 import sys
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Subset
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+from train import Net
 
 
-# class definitions
-class MyNetwork(nn.Module):
-    def __init__(self):
-        pass
+# function to test the network on first 10 testing images
+def test_network(
+    network: nn.Module,
+    test_loader: DataLoader,
+):
+    # set the network to evaluation mode
+    network.eval()
+    # store the predicted labels
+    predicted_labels = []
 
-    # computes a forward pass for the network
-    # methods need a summary comment
-    def forward(self, x):
-        return x
+    # for each image in the testing data
+    with torch.no_grad():
+        for idx, (data, target) in enumerate(test_loader):
+            # compute the output of the network
+            output = network(data)
+            # print the output, rounded to 2 decimal places
+            print(
+                "Image {} output: {}".format(
+                    idx + 1, [round(o.item(), 2) for o in output[0]]
+                )
+            )
+            # get the index of the max log-probability
+            pred = output.data.max(1, keepdim=True)[1]
+            # print the prediction
+            print("Image {} prediction: {}".format(idx + 1, pred[0][0]))
+            predicted_labels.append(pred[0][0])
+            # print the label
+            print("Image {} label: {}".format(idx + 1, target.item()))
+
+            # plot only the first nine images
+            if idx == 9:
+                continue
+            # plot each image in the grid
+            plt.subplot(3, 3, idx + 1)
+            # plot the image in grayscale
+            plt.imshow(data.squeeze().numpy(), cmap="gray", interpolation="none")
+            # plot the label as the title of the image
+            plt.title("Prediction {}".format(predicted_labels[idx]), y=0.95)
+            # do not show the axes
+            plt.xticks([])
+            plt.yticks([])
+
+    plt.show()
 
 
-# useful functions with a comment for each function
-def train_network(arguments):
-    return
-
-
-# main function (yes, it needs a comment too)
+# main function
 def main(argv):
-    # handle any command line arguments in argv
+    # import MNIST testing data
+    testing_data = datasets.MNIST(
+        root="./data",
+        train=False,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+    # create a subset of the testing data with the first 10 images
+    subset_testing_data = Subset(testing_data, range(10))
+    # create a data loader for the testing data
+    test_loader = DataLoader(subset_testing_data, batch_size=1)
 
-    # main function code
+    # read the model from file
+    network = Net()
+    network.load_state_dict(torch.load("./results/model.pth"))
+
+    # test the network on the first 10 images
+    test_network(network, test_loader)
+
     return
 
 
--- train.py
+++ train.py
@@ -4,30 +4,208 @@
 
 
 # import statements
+import os
 import sys
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
 
 
-# class definitions
-class MyNetwork(nn.Module):
+# define the convolutional neural network
+class Net(nn.Module):
+    # function to initialize the layers of the network
     def __init__(self):
-        pass
+        super(Net, self).__init__()
+        # conv1 is a convolutional layer with 10 5x5 filters
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        # conv2 is a convolutional layer with 20 5x5 filters
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # this is a dropout layer with a probability of 0.5
+        self.dropout = nn.Dropout2d()
+        # fc1 is a fully connected layer with 50 neurons
+        self.fc1 = nn.Linear(320, 50)
+        # fc2 is a fully connected layer with 10 neurons
+        self.fc2 = nn.Linear(50, 10)
 
-    # computes a forward pass for the network
-    # methods need a summary comment
+    # function to compute a forward pass for the network
     def forward(self, x):
+        # x is 1x28x28. conv1 is applied here
+        x = self.conv1(x)
+        # x is 10x24x24
+        # a max pooling layer with a 2x2 window and a ReLU function are applied here
+        x = F.relu(F.max_pool2d(x, 2))
+        # x is 10x12x12. conv2 is applied here
+        x = self.conv2(x)
+        # x is 20x8x8. a dropout layer is applied here
+        x = self.dropout(x)
+        # x is 20x8x8
+        # a max pooling layer with a 2x2 window and a ReLU function are applied here
+        x = F.relu(F.max_pool2d(x, 2))
+        # x is 20x4x4. x is flattened here
+        x = x.view(-1, 320)
+        # x is 320x1. fc1 and a ReLU function are applied here
+        x = F.relu(self.fc1(x))
+        # x is 50x1. fc2 and a log softmax function are applied here
+        x = F.log_softmax(self.fc2(x))
+        # x is 10x1
         return x
 
 
-# useful functions with a comment for each function
-def train_network(arguments):
-    return
+# function to train the network
+def train_network(
+    network: Net,
+    optimizer: optim.SGD,
+    epoch: int,
+    train_loader: DataLoader,
+    train_losses: list,
+    train_counter: list,
+):
+    # set the network to training mode
+    network.train()
+    # for each batch in the training data (64 images per batch)
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # set the gradients to zero
+        optimizer.zero_grad()
+        # compute the output of the network
+        output = network(data)
+        # compute the loss
+        loss = F.nll_loss(output, target)
+        # compute the gradients
+        loss.backward()
+        # update the weights
+        optimizer.step()
+        # for every 10 batches
+        if batch_idx % 10 == 0:
+            # print the loss
+            print(
+                "Train epoch {} [{}/{} ({:.0f}%)]\t\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
+            # save the loss and counter
+            train_losses.append(loss.item())
+            train_counter.append(
+                (batch_idx * 64) + ((epoch - 1) * len(train_loader.dataset))
+            )
+            # create ./results if it does not exist
+            if not os.path.exists("./results"):
+                os.makedirs("./results")
+            # save the model and optimizer
+            torch.save(network.state_dict(), "./results/model.pth")
+            torch.save(optimizer.state_dict(), "./results/optimizer.pth")
 
 
-# main function (yes, it needs a comment too)
+# function to test the network
+def test_network(
+    network: Net,
+    test_loader: DataLoader,
+    test_losses: list,
+):
+    # set the network to evaluation mode
+    network.eval()
+    # initialize the test loss and correct counter
+    test_loss = 0
+    correct = 0
+    # for each batch in the testing data (1000 images per batch)
+    with torch.no_grad():
+        for data, target in test_loader:
+            # compute the output of the network
+            output = network(data)
+            # compute the loss
+            test_loss += F.nll_loss(output, target, size_average=False).item()
+            # get the index of the max log-probability
+            pred = output.data.max(1, keepdim=True)[1]
+            # increment the correct counter if the prediction is correct
+            correct += pred.eq(target.data.view_as(pred)).sum()
+    # compute the average test loss
+    test_loss /= len(test_loader.dataset)
+    # append the test loss to the list
+    test_losses.append(test_loss)
+    # print the test loss and accuracy
+    print(
+        "\nTest average loss: {:.4f}\t\tAccuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
+        )
+    )
+
+
+# main function
 def main(argv):
-    # handle any command line arguments in argv
+    # import MNIST training data
+    training_data = datasets.MNIST(
+        root="./data",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+    # create a data loader for the training data
+    train_loader = DataLoader(training_data, batch_size=64, shuffle=True)
+    # show the first six example digits
+    for i in range(6):
+        # plot each image in the grid
+        plt.subplot(2, 3, i + 1)
+        # plot the image in grayscale
+        plt.imshow(training_data.data[i], cmap="gray", interpolation="none")
+        # plot the label as the title of the image
+        plt.title("Label {}".format(training_data.targets[i]))
+        # do not show the axes
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
 
-    # main function code
+    # import MNIST testing data
+    testing_data = datasets.MNIST(
+        root="./data",
+        train=False,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+    # create a data loader for the testing data
+    test_loader = DataLoader(testing_data, batch_size=1000, shuffle=True)
+
+    # initialize the network
+    network = Net()
+    # initialize the optimizer
+    optimizer = optim.SGD(network.parameters(), lr=0.01, momentum=0.5)
+    # train the network for 5 epochs
+    n_epochs = 5
+    # initialize the training and testing losses and counters
+    train_losses = []
+    train_counter = []
+    test_losses = []
+    test_counter = [i * len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+    # test the network before training
+    test_network(network, test_loader, test_losses)
+    # train the network for 5 epochs
+    for epoch in range(1, n_epochs + 1):
+        train_network(
+            network, optimizer, epoch, train_loader, train_losses, train_counter
+        )
+        test_network(network, test_loader, test_losses)
+
+    # plot the training loss
+    plt.plot(train_counter, train_losses, color="blue")
+    plt.scatter(test_counter, test_losses, color="red")
+    plt.legend(["Train Loss", "Test Loss"], loc="upper right")
+    plt.xlabel("number of training examples seen")
+    plt.ylabel("negative log likelihood loss")
+    plt.show()
+
     return
 
 
